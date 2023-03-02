27b995595a1dfd21ccc9625f6638f432ac13e71e
ale-liang <liang.alex919@gmail.com>
2023-03-02 17:33:52 -0600

    Replaced second clock with 100MHz clk and a count variable for each second in the parking meter controller module

--- Lab_4/Lab_4.srcs/sources_1/new/Controller.v
+++ Lab_4/Lab_4.srcs/sources_1/new/Controller.v
@@ -33,12 +33,13 @@ module Controller(
     output ZeroTimeLeft
     );
     
+    //Registers for tracking each second
+    parameter secPeriod = 100000000;
+    reg [25:0] cnt = 0;
+    
     //Wire variables for output of debounced buttons
     wire U, L, R, D;
-    
-    //Create register for second clock
-    reg secClk;
-    
+        
     //Create registers to hold output values
     reg [15:0] TimeLeftCntReg = 0;
     reg LessThan200Reg, ZeroTimeLeftReg = 1;
@@ -55,53 +56,60 @@ module Controller(
     ButtonDebounce bR(Clk, ButtR, R);
     ButtonDebounce bD(Clk, ButtD, D);
     
-    //Instantiate a clock that generates a pulse every second
-    SecGen(Clk, secClk);
-    
-    always @(posedge secClk) begin
-        //if SWITCH 0 is on, then display is stuck at 10 until switch 0 is off
-        if(Sw0 == 1'b1) begin
-            TimeLeftCntReg <= 10;
-            LessThan200Reg <= 1'b1;
-            ZeroTimeLeftReg <= 1'b0;
-        end
-        //if SWITCH 1 is on, then display is stuck at 205 until switch is off
-        else if (Sw1 == 1'b1 || Sw0 == 1'b0) begin
-            TimeLeftCntReg <= 205;
-            LessThan200Reg <= 1'b0;
-            ZeroTimeLeftReg <= 1'b0;
-        end 
-        //Else decrement as normal and check if any of the buttons have been pressed
-        //and increment according to button pressed
-        else begin
-            //Checks if Time left on Parking meter is Greater than 0
-            //If true, decrement and set proper outputs
-            //Else, time is 0 and no decrement
-            if(TimeLeftCntReg > 0) begin
-                ZeroTimeLeftReg <= 1'b0;
-                TimeLeftCntReg <= TimeLeftCntReg - 1;
-                //time less than 200 seconds -> output for LessThan200 is true
-                if(TimeLeftCntReg < 200) begin
+    always @(posedge Clk) begin
+        //Decrements if cnt == secPeriod (which means a second has passed)
+        //and also updates the outputs
+        if (cnt == secPeriod) begin
+                //Checks if Time left on Parking meter is Greater than 0
+                //If true, decrement and set proper outputs
+                //Else, time is 0 and no decrement
+                if(TimeLeftCntReg > 0) begin
+                    ZeroTimeLeftReg <= 1'b0;
+                    TimeLeftCntReg <= TimeLeftCntReg - 1;
+                    //time less than 200 seconds -> output for LessThan200 is true
+                    if(TimeLeftCntReg < 200) begin
+                        LessThan200Reg <= 1'b1;
+                    end
+                    else begin
+                        LessThan200Reg <= 1'b0;
+                    end        
+                end           
+                else begin
+                    ZeroTimeLeftReg <= 1'b1;
                     LessThan200Reg <= 1'b1;
                 end
-                else begin
-                    LessThan200Reg <= 1'b0;
-                end        
-            end           
-            else begin
-                ZeroTimeLeftReg <= 1'b1;
+            cnt = 1;
+        end
+        //Else if the cnt doesn't equal the secPeriod, increment cnt and update TimeLeftCntReg according to buttons and switches on
+        else begin
+            cnt = cnt + 1;
+            //if SWITCH 0 is on, then display is stuck at 10 until switch 0 is off
+            if(Sw0 == 1'b1) begin
+                TimeLeftCntReg <= 10;
                 LessThan200Reg <= 1'b1;
+                ZeroTimeLeftReg <= 1'b0;
             end
-            //Checks if any of the buttons have been pressed
-            //if true, increment according to button value
-            if(U)
-                TimeLeftCntReg <= TimeLeftCntReg + 10;
-            if(L)
-                TimeLeftCntReg <= TimeLeftCntReg + 180;
-            if(R)
-                TimeLeftCntReg <= TimeLeftCntReg + 200;
-            if(D)
-                TimeLeftCntReg <= TimeLeftCntReg + 550;            
+            //if SWITCH 1 is on, then display is stuck at 205 until switch is off
+            else if (Sw1 == 1'b1 || Sw0 == 1'b0) begin
+                TimeLeftCntReg <= 205;
+                LessThan200Reg <= 1'b0;
+                ZeroTimeLeftReg <= 1'b0;
+            end 
+            //Else decrement as normal and check if any of the buttons have been pressed
+            //and increment according to button pressed
+            else begin     
+                
+                //Checks if any of the buttons have been pressed
+                //if true, increment according to button value
+                if(U)
+                    TimeLeftCntReg <= TimeLeftCntReg + 10;
+                if(L)
+                    TimeLeftCntReg <= TimeLeftCntReg + 180;
+                if(R)
+                    TimeLeftCntReg <= TimeLeftCntReg + 200;
+                if(D)
+                    TimeLeftCntReg <= TimeLeftCntReg + 550;
+             end                   
         end
         
         //If Time left on meter is > 9999, time left will default to 9999
@@ -110,33 +118,7 @@ module Controller(
         end    
     end    
     
+    
+    
 endmodule
 
-//Module generates a pulse with a period of 1 second
-module SecGen(
-    input Clk,
-    output Seconds
-    );
-    
-    parameter secPeriod = 50000000;
-    
-    reg [25:0] cnt;
-    reg secPulse;
-    
-    assign Seconds = secPulse;
-    
-    initial begin
-        cnt = 0;
-        secPulse = 0;
-    end
-    
-    always@(posedge Clk) begin
-        if (cnt == secPeriod) begin
-            secPulse = ~secPulse;
-            cnt = 1;
-        end
-        else
-            cnt = cnt + 1;
-        end
-    
-endmodule
