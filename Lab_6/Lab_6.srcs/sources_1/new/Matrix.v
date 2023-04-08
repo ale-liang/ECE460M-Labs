d0f14ffdc4ed831d79f439fda72e494f88c04d10
ale-liang <liang.alex919@gmail.com>
2023-04-08 13:41:13 -0500

    Implemented some of Matrix and floating point addition

--- Lab_6/Lab_6.srcs/sources_1/new/Matrix.v
+++ Lab_6/Lab_6.srcs/sources_1/new/Matrix.v
@@ -53,8 +53,35 @@ module Matrix(
     output [7:0] c22,
     output reg done
     );
+      
+    wire [7:0] startMAC;
     
-    wire [7:0] a[2:0][2:0];
-    wire [7:0] b[2:0][2:0];
+    reg [7:0] row0Input; //top left
+    reg [7:0] col0Input;
+    reg [7:0] row1Input; //middle left/top
+    reg [7:0] col1Input;
+    reg [7:0] row2Input; //bot left/top right
+    reg [7:0] col2Input;
+    
+    // MAPPING OUT THE Matrix
+    //        c0 c1 c2
+    //     r0 00 01 02
+    //     r1 10 11 12
+    //     r2 20 21 22
+    
+    wire [7:0] r00to01, r01to02, r10to11, r11to12, r20to21, r21to22; //connections between rows
+    wire [7:0] c00to10, c10to20, c01to11, c11to21, c02to12, c12to22; //connections between oolumns    
+    
+    //doesn't need all outputs for passing to be filled out at the end of rows and columns
+    MAC m_c00(clk, row0Input, col0Input, startMAC[0], c00, r00to01, c00to10);
+    MAC m_c01(clk, r00to01, col1Input, startMAC[1], c01, r01to02, c01to11);
+    MAC m_c02(clk, r01to02, col2Input, startMAC[2], c02, ,c02to12); //doesn't need to pass A
+    MAC m_c10(clk, row1Input, c00to10, startMAC[3], c10, r10to11, c10to20);
+    MAC m_c11(clk, r10to11, c01to11, startMAC[4], c11, r11to12, c11to21); 
+    MAC m_c12(clk, r11to12, c02to12, startMAC[5], c12, , c12to22); //doesn't need to pass A
+    MAC m_c20(clk, row2Input, c10to20, startMAC[6], c20, r20to21, ); //doesn't need to pass B 
+    MAC m_c21(clk, r20to21, c11to21, startMAC[7], c21, r21to22, ); //doesn't need to pass B
+    MAC m_c22(clk, r21to22, c12to22, startMAC[8], c22, ,); //doesn't need to pass A and B
+    
     
 endmodule
--- Lab_6/Lab_6.srcs/sources_1/new/fp_adder.v
+++ Lab_6/Lab_6.srcs/sources_1/new/fp_adder.v
@@ -23,17 +23,43 @@
 module fp_adder(
     input [7:0] a,
     input [7:0] b,
-    output reg [7:0] out
+    output [7:0] out
     );
     
-    reg [2:0] exp_diff;
+    reg sOut;
+    reg comp;
+    reg [3:0] eOut;
+    reg [9:0] mOut, mA, mB;
+    
+    initial begin
+        sOut = 1'b0;
+        eOut = 4'b0;
+        mOut = 10'b0;
+        
+        mA = {1'b0, 1'b1, a[3:0], 4'b0};
+        mB = {1'b0, 1'b1, b[3:0], 4'b0};
+    end
+    
     
     always @(*) begin
     
-        if(a == 7'b0) begin
-            
+        if(a == 8'b0) begin
+            eOut <= {1'b0, b[6:4]};
+            sOut <= b[7];
+            mOut[7:4] <= b[3:0];
+        end else if (b == 8'b0) begin
+            eOut <= {1'b0, a[6:4]};
+            sOut <= a[7];
+            mOut[7:4] <= a[3:0];
+        end else if ((a[6:0] == b[6:0]) && (a[7] ^ b[7] == 1'b1)) begin
+            eOut <= 4'b0;
+            sOut <= 1'b0;
+            mOut <= 10'b0;
         end
+        
     end
+    
+    assign out = {sOut, eOut[2:0], mOut[7:4]};
     
     
     
