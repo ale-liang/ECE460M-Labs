3a77906f22d2e6754887b86acc3e261ae4e4e5fd
JT-Hamada <60447422+JT-Hamada@users.noreply.github.com>
2023-04-09 19:00:44 -0500

    Add files via upload
    half working adder

--- Lab_6/Lab_6.srcs/sources_1/new/fp_adder.v
+++ Lab_6/Lab_6.srcs/sources_1/new/fp_adder.v
@@ -29,38 +29,60 @@ module fp_adder(
     reg sOut;
     reg comp;
     reg [3:0] eOut;
-    reg [9:0] mOut, mA, mB;
+    reg [9:0] fOut, fA, fB;
+    integer i;
     
     initial begin
         sOut = 1'b0;
         eOut = 4'b0;
-        mOut = 10'b0;
-        
-        mA = {1'b0, 1'b1, a[3:0], 4'b0};
-        mB = {1'b0, 1'b1, b[3:0], 4'b0};
+        fOut = 10'b0;
     end
     
     
     always @(*) begin
-    
-        if(a == 8'b0) begin
-            eOut <= {1'b0, b[6:4]};
-            sOut <= b[7];
-            mOut[7:4] <= b[3:0];
-        end else if (b == 8'b0) begin
-            eOut <= {1'b0, a[6:4]};
-            sOut <= a[7];
-            mOut[7:4] <= a[3:0];
-        end else if ((a[6:0] == b[6:0]) && (a[7] ^ b[7] == 1'b1)) begin
-            eOut <= 4'b0;
-            sOut <= 1'b0;
-            mOut <= 10'b0;
+        fA = {1'b0, a[3:0], 5'b0};
+        fB = {1'b0, b[3:0], 5'b0};
+        
+        //Compare Exponents
+        if (a[6:4] > b[6:4]) begin //shift a until expA == expB
+            fB <= fB >> (a[6:4]-b[6:4]);
+            eOut <= a[6:4];
+        end else if (b[6:4] > a[6:4]) begin //shift b until expA == expB
+            fA <= fA >> (b[6:4]-a[6:4]);
+            eOut <= b[6:4];
+        end //else expA == expB so do nothing
+        
+        //Add Fractions
+        if (a[7] ~^ b[7]) begin //signs are the same- add fractA + fractB
+            fOut = fA + fB;
+            sOut = a[7];
+        end else if (a[7] == 1) begin //subtract -fractA + fractB
+            fOut = fB + (-fA);
+            sOut = (fA > fB) ? 1 : 0;
+        end else begin //subtract fractA - fractB
+            fOut = fA + (-fB);
+            sOut = (fB > fA) ? 1 : 0;
         end
         
+        if (fOut == 10'b0) begin //If Result is 0, Set Excponent to 0
+            eOut = 4'b0;
+            sOut = 1'b0;
+        end 
+        
+        //Check for fraction overflow and Normalize Fraction
+        if(fOut[9] == 1'b1) begin
+            fOut <= fOut >> 1;
+            eOut <= eOut + 1;
+        end else begin
+            for(i = 0; i < 10; i = i + 1) begin
+                if (fOut[8] != 1'b1) begin
+                    fOut <= fOut << 1;
+                    eOut <= eOut - 1;
+                end
+            end    
+        end
     end
     
-    assign out = {sOut, eOut[2:0], mOut[7:4]};
-    
-    
+    assign out = {sOut, eOut[2:0], fOut[7:4]};
     
 endmodule
